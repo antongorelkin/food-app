a681ade269d1c3968de0a08cd6674020643aea85
Anton Gorelkin <lucky42ru@mail.ru>
2026-05-21 19:15:50 +0200

    some changes

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import { Session } from "@supabase/supabase-js";
 import FridgeGrid from "./Fridge/FridgeGrid";
